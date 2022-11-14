6d263aea216c2853a924de28b73563f01e2267c9
utku <bayraktarogluutku@gmail.com>
2022-11-14 03:55:01 +0300

    js ile ses oynatma öğrenildi - bitiş

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 
 document.addEventListener("keydown", getKey)
 
+function getKey(e) {
 
+    $.each($('.keyWhite'), function (indexInArray, valueOfElement) { 
+         if(valueOfElement.innerHTML.includes(e.key)){
+            var keyView = $(valueOfElement)
+            keyView.animate({height: '161px'}, "fast")
+            keyView.animate({height: '10rem'}, "fast")
 
- function getKey(e) {
-   
+         }
+    });
 
     const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-
- 
-
     audio.currentTime = 0;
     audio.play();
-
+    
 }
